acf7acf7f9481d11a499046b652d01d508673999
juaniwk3 <juaniwk3@gmail.com>
2022-12-27 23:01:45 -0300

    remove target

--- python-tcp-chat/client.py
+++ python-tcp-chat/client.py
@@ -1,15 +1,10 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("", 8080))
-
-name = ""
-while name == "":
-    name = input("Enter your name: ")
+client.connect(("localhost", 8080))
 
 while True:
-    msg = input("%s: " % name)
 
-    msg = name + ": " + msg
+    msg = input("you: ")
 
     client.send(msg.encode())
--- python-tcp-chat/server.py
+++ python-tcp-chat/server.py
@@ -1,7 +1,7 @@
 import socket
 from _thread import *
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = ""
 port = 8080
@@ -10,17 +10,20 @@ try:
     s.bind((host, port))
 except socket.error as e:
     print(str(e))
-print('Socket is listening on port %s..' % port)
+print('Socket is listening..')
 s.listen(5)
 
 
 def multi_threaded_client(conn):
     while True:
         data = conn.recv(1024)
-        print(data.decode('utf-8'))
+        if not data:
+            break
+        print(data.decode(), end="")
     conn.close()
 
 
 while True:
     Client, address = s.accept()
     start_new_thread(multi_threaded_client, (Client, ))
+Client.close()
